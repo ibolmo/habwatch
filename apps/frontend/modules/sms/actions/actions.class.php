fffdd151379ca388988ef082bc2d2e2fcc94a353
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-02 12:03:29 -0800

    Initial commit for sms module

---
+++ apps/frontend/modules/sms/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * sms actions.
+ *
+ * @package    merhab
+ * @subpackage sms
+ * @author     Olmo Maldonado, <ibolmo@ucla.edu>
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class smsActions extends sfActions
+{
+}
---
+++ test/functional/frontend/smsActionsTest.php
@@ -0,0 +1,7 @@
+<?php
+
+include(dirname(__FILE__).'/../../bootstrap/functional.php');
+
+$b = new sfTestFunctional(new sfBrowser());
+$t = $b->test();
+
