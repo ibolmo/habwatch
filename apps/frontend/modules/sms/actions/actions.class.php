8e7fc7bab1961d9f0b5cafdafe6ac2e9fe5677c3
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-03 13:58:17 -0800

    Finished? worked with sms module

--- apps/frontend/modules/sms/actions/actions.class.php
+++ apps/frontend/modules/sms/actions/actions.class.php
@@ -10,4 +10,49 @@
  */
 class smsActions extends sfActions
 {
+    static $help = '
+Usage:
+  mh [cmd] [qty] cond subj loc
+Where each can be:
+  cmd: report, list, help, note
+  qty: 1, 2, ... , five, six ...
+  cond: dead, sick, ...
+  subj: fish, ...';
+    
+    public function preExecute()
+    {
+        $this->PhoneNumber = $this->User = false;
+        
+        if ($in_number = $this->getRequest()->getParameter('s_phone_number')) {
+            $this->PhoneNumber = Doctrine::getTable('PhoneNumber')->findOneByNumber(sfValidatorPhoneNumber::cleanPhoneNumber($in_number));
+            if ($this->PhoneNumber) $this->User = $this->PhoneNumber->Profile->User;
+        }
+        
+        $this->getResponse()->setContentType('text/plain');
+    }
+    
+    public function executeParse(sfRequest $request)
+    {
+        $this->forwardIf(!$this->User, 'sms', 'error400');
+        $this->setVar('parsed', self::parse($request->getParameter('message', '')));
+        $this->forward('message', $parsed['command']);
+    }
+    
+    public static function parse($message)
+    {
+        return array('command' => 'help');
+    }
+    
+    public function executeError400(sfRequest $request)
+    {
+        $this->setLayout(false);
+        $this->getResponse()->setStatusCode(400);
+        return $this->renderText('Your phone number is not registered');
+    }
+    
+    public function executeHelp()
+    {
+        $this->setLayout(false);
+        return $this->renderText(self::$help);
+    }
 }
--- test/functional/frontend/smsActionsTest.php
+++ test/functional/frontend/smsActionsTest.php
@@ -1,7 +1,33 @@
 <?php
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
+define('SMS', '/sms');
+define('IN_PHONE', '9568215736');
 
-$b = new sfTestFunctional(new sfBrowser());
+/**
+* 
+*/
+class SMSTest extends sfTestBrowser
+{
+    static $uri = '/sms';
+    
+    public function sms($message, $from = IN_PHONE, $to = '')
+    {
+        return $this->get(self::$uri."/$from/$message/$to");
+    }
+}
+
+$b = new SMSTest();
 $t = $b->test();
 
+$b->get(SMSTest::$uri)
+  ->isStatusCode(404);
+  
+$b->sms('my message', 5555555555)
+  ->isStatusCode(400)
+  ->responseContains('not registered');
+
+$b->sms('help');
+$t->is($b->getResponse()->getContentType(), 'text/plain; charset=utf-8', 'Content type is plain text');
+$b->responseContains('Usage:');
+  
