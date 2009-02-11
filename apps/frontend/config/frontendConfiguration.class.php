bcc32dd9f4802717edeb22f8eaa6a46415547934
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-10 22:11:47 -0800

    Some cleanup, but I was trying to get frontendConfiguration.class.php to be portable. Still having a bug

--- apps/frontend/config/frontendConfiguration.class.php
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -3,9 +3,8 @@
 class frontendConfiguration extends sfApplicationConfiguration
 {
     public function configure()
-    {
-        if ($phlickr_lib_dir = sfConfig::get('app_phlickr_lib_dir'))
-        {
+    {       
+        if ($phlickr_lib_dir = 'C:/www/habwatch/lib/vendor/phlickr/') {
             set_include_path($phlickr_lib_dir.PATH_SEPARATOR.get_include_path());
         }
     }
