1172c4e2f59c4d595e61936d84c1398aca45af9e
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-11 16:12:28 -0800

    Some css work

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,6 +1,6 @@
 <div class="block">
 	<?php if (Flickr::hasPhotos()): ?>
-	    <iframe id="map" name="map" src="http://www.flickr.com/photos/35147405@N02/map" style="margin-top: -60px" width="100%" height="800" marginwidth="0" marginheight="0" frameborder="no" scrolling="no">Please upgrade your browser</iframe>
+	    <iframe id="map" name="map" src="http://www.flickr.com/photos/35147405@N02/map" marginwidth="0" marginheight="0" frameborder="no" scrolling="no">Please upgrade your browser</iframe>
 	<?php else: ?>
 	    <h2>Welcome to <?php echo sfConfig::get('app_project_name') ?>!</h2>
 	    <?= include_partial('default/intro') ?>
