028d85b149daebfa3779787e110e3aea59793107
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-14 01:30:09 -0800

    Figured out the frontendConfiguration problem: use initialize method, and call parent first

--- apps/frontend/config/frontendConfiguration.class.php
+++ apps/frontend/config/frontendConfiguration.class.php
@@ -1,10 +1,12 @@
 <?php
-require_once(dirname(__FILE__).'/../../../config/app.php');
+
 class frontendConfiguration extends sfApplicationConfiguration
 {
-    public function configure()
-    {       
-        if ($phlickr_lib_dir = trim(PHLICKR_LIB_DIR)) {
+    public function initialize()
+    {
+        parent::initialize();
+        
+        if ($phlickr_lib_dir = sfConfig::get('app_phlickr_lib_dir')) {
             set_include_path($phlickr_lib_dir.PATH_SEPARATOR.get_include_path());
         }
     }
