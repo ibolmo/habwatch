ad49419abac008879dd8abee78dac7604e26caa9
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-25 19:46:03 -0800

    Intermediate commit, I'm mad that I can't figure out how to do optional uri

--- apps/frontend/modules/email/actions/actions.class.php
+++ apps/frontend/modules/email/actions/actions.class.php
@@ -10,13 +10,8 @@
  */
 class emailActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->forward('default', 'module');
-  }
+    public function executeCreate(sfRequest $request)
+    {
+        
+    }
 }
