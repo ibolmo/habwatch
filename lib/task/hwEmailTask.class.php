e4397a5a2c11016191fb8596099b430363499ca2
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-02-25 19:43:39 -0800

    Intermediate commit, I'm mad that I can't figure out how to do optional uri

--- lib/task/hwEmailTask.class.php
+++ lib/task/hwEmailTask.class.php
@@ -1,14 +1,14 @@
 <?php
 
-class hwEmailTask extends sfBaseTask
+class hwEmailTask extends sfDoctrineBaseTask
 {
     protected function configure()
     {
         $this->addOptions(array(
-            new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
-            new sfCommandOption('uri', null, sfCommandOption::PARAMETER_OPTIONAL, 'The URI to POST to', '@email_post'),
-            new sfCommandOption('email-address', null, sfCommandOption::PARAMETER_OPTIONAL, 'Email address of the IMAP Mailbox'),
-            new sfCommandOption('password', null, sfCommandOption::PARAMETER_OPTIONAL, 'Password of the IMAP Mailbox', sfConfig::get('app_smtp_password'))
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', 'frontend'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('uri', null, sfCommandOption::PARAMETER_REQUIRED, 'The URI to POST to'),
+            new sfCommandOption('mail-connection', null, sfCommandOption::PARAMETER_OPTIONAL, 'URI Abstraction (see Net_Url.php) for mailbox connection'),
         ));
         
         $this->aliases = array('habwatch-email-pull');
@@ -24,9 +24,48 @@ EOF;
 
     protected function execute($arguments = array(), $options = array())
     {
-        if (!$options['email-address']) $options['email-address'] = sfConfig::get('app_smtp_username');
-        if (!$options['password']) $options['password'] = sfConfig::get('app_smtp_password');
+        if (!$options['mail-connection']) $options['mail-connection'] = sfConfig::get('app_imap_uri') or die('Missing mail-connection');
         
+        require_once 'Mail/IMAPv2.php';  
+        $mail = new Mail_IMAPv2($options['mail-connection']);
+        if ($alerts = $mail->alerts(false)) $this->logSection('email', $alerts);
+        if ($errors = $mail->errors(false)) $this->logSection('email', $errors, null, 'ERROR');
         
+        if ($count = $mail->messageCount()) {
+            $browser = new sfWebBrowser();
+            $this->logSection('email', "You have $count messages");
+            
+            for ($mid = 1; $mid  <= $count; $mid++) {
+                $mail->getHeaders($mid);
+                $header = $mail->header[$mid];
+                                
+                $mail->getParts($mid);
+                $msg = $mail->msg[$mid];
+                
+                $browser->post($options['uri'], array(
+                    'from' => $header['fromaddress'],
+                    'to' => $header['toaddress'],
+                    'reply-to' => $header['reply_toaddress'],
+                    'subject' => $header['subject'],
+                    'timestamp' => $header['udate'],
+                    'message-id' => $header['message-id'],
+                    'photos' => array(
+                        'filename1' => '@photo',
+                        'filename2' => '@photo',
+                    )
+                ));
+                
+                //*
+                var_dump(array(
+                    'result' => $browser->getResponse()
+                ));
+                //*/
+            }
+        } else {
+            $this->logSection('email', 'No new messages');
+        }
+        
+        $mail->close();
+        if ($mail->error) $this->logBlock($mail->error->getErrors(), 'ERROR');
     }
 }
