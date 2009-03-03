aca397482694fae4d0a2dc61cd1bb6d39fe3c252
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-03-02 16:05:01 -0800

    backend default init

---
+++ apps/backend/modules/default/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    habwatch
+ * @subpackage default
+ * @author     Olmo Maldonado, <ibolmo@ucla.edu>
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class defaultActions extends sfActions
+{
+    /**
+    * Executes index action
+    *
+    * @param sfRequest $request A request object
+    */
+    public function executeIndex(sfWebRequest $request)
+    {
+    }
+}
