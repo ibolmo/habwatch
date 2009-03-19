0f1a6a321511e5d68f7d6ad345705cbce37ee16d
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-03-19 16:04:13 -0700

    Added a default case for when the user has lacked parcticipation

--- apps/frontend/modules/message/templates/_list.php
+++ apps/frontend/modules/message/templates/_list.php
@@ -1,23 +1,29 @@
-<?= include_partial('sms/list', array('sms' => $SMSPager->getResults() )) ?>
-<?php if ($SMSPager->haveToPaginate()): ?>
-    <div class="pagination">
-        <a href="<?= url_for('@messages') ?>?sms_page=1">&laquo;</a>
-        <a href="<?= url_for('@messages') ?>?sms_page=<?= $SMSPager->getPreviousPage() ?>">&lsaquo;</a>
-        
-        <?php foreach ($SMSPager->getLinks() as $page): ?>
-            <?php if ($page == $SMSPager->getPage()): ?>
-                <?= $page ?>
-            <?php else: ?>
-                <a href="<?= url_for('@messages') ?>?sms_page=<?= $page ?>"><?= $page ?></a>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        
-        <a href="<?= url_for('@messages') ?>?sms_page=<?= $SMSPager->getNextPage() ?>">&rsaquo;</a>
-        <a href="<?= url_for('@messages') ?>?sms_page=<?= $SMSPager->getLastPage() ?>">&raquo;</a>
-    </div>
-<?php endif; ?>
-
-<?php if (isset($emails) && $emails->count()): ?>
-    <hr />
-    <?= include_partial('email/list', array('emails' => $emails )) ?>
+<?php if ($SMSPager->getNbResults()): ?>
+    <?= include_partial('sms/list', array('sms' => $SMSPager->getResults() )) ?>
+    <?php if ($SMSPager->haveToPaginate()): ?>
+        <div class="pagination">
+            <a href="<?= url_for('@messages') ?>?sms_page=1">&laquo;</a>
+            <a href="<?= url_for('@messages') ?>?sms_page=<?= $SMSPager->getPreviousPage() ?>">&lsaquo;</a>
+            
+            <?php foreach ($SMSPager->getLinks() as $page): ?>
+                <?php if ($page == $SMSPager->getPage()): ?>
+                    <?= $page ?>
+                <?php else: ?>
+                    <a href="<?= url_for('@messages') ?>?sms_page=<?= $page ?>"><?= $page ?></a>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            
+            <a href="<?= url_for('@messages') ?>?sms_page=<?= $SMSPager->getNextPage() ?>">&rsaquo;</a>
+            <a href="<?= url_for('@messages') ?>?sms_page=<?= $SMSPager->getLastPage() ?>">&raquo;</a>
+        </div>
+    <?php endif; ?>
+    
+    <?php if (isset($emails) && $emails->count()): ?>
+        <hr />
+        <?= include_partial('email/list', array('emails' => $emails )) ?>
+    <?php endif ?>
+<?php else: ?>
+    <p>
+        You have not contributed <strong> SMS </strong> messages. See the <?= link_to('instructions', '@instructions') ?> to participate.
+    </p>
 <?php endif ?>
