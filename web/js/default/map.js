4a6e6b461e040de34e16768a03e1a7c8a2bea61c
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-04-01 17:43:17 -0700

    Updated map height, and cleaned map.js and getting ready for loading screen

--- web/js/default/map.js
+++ web/js/default/map.js
@@ -1,75 +1,74 @@
 window.addEvent('domready', function(){
-	
-	var map = new OpenLayers.Map('map', {
-		maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
+    
+    var map = new OpenLayers.Map ('map', {
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.Attribution()
+        ],
+        maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34),
         maxResolution: 156543.0399,
-        numZoomLevels: 20,
-    	displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        numZoomLevels: 19,
         units: 'm',
-        theme: null,
-	    controls: [
-		    new OpenLayers.Control.Navigation(),
-			new OpenLayers.Control.ArgParser(),
-			new OpenLayers.Control.Attribution(),
-		]
-	});
-	
-	var osm = new OpenLayers.Layer.OSM.Mapnik('OSM', {
-		displayOutsideMaxExtent: true,
-	    wrapDateLine: true,
-	    renderers: ['SVG', 'Canvas', 'VML']
-	});
-	osm.url = ([
-		'http://a.tile.cloudmade.com/9ea55b2fb8ff5059accd298987268530/4/256/',
-		'http://b.tile.cloudmade.com/9ea55b2fb8ff5059accd298987268530/4/256/',
-		'http://c.tile.cloudmade.com/9ea55b2fb8ff5059accd298987268530/4/256/'
-	]);
+        projection: new OpenLayers.Projection('EPSG:900913'),
+        displayProjection: new OpenLayers.Projection('EPSG:4326')
+    });
+
+    var osm = new OpenLayers.Layer.OSM.Mapnik('OSM');
+    osm.url = ([
+        'http://a.tile.cloudmade.com/9ea55b2fb8ff5059accd298987268530/4/256/',
+        'http://b.tile.cloudmade.com/9ea55b2fb8ff5059accd298987268530/4/256/',
+        'http://c.tile.cloudmade.com/9ea55b2fb8ff5059accd298987268530/4/256/'
+    ]);
     map.addLayer(osm);
     map.setCenter(new OpenLayers.LonLat(-118.489609, 33.757989).transform(new OpenLayers.Projection('EPSG:4326'), map.getProjectionObject()), 10);
     
-    var photos = new OpenLayers.Layer.Vector("Photos", {
+    var photos = new OpenLayers.Layer.Vector('Photos', {
 	    strategies: [
-	        new OpenLayers.Strategy.Fixed(),
+	        new OpenLayers.Strategy.Fixed({ preload: true }), //should use BBox
 	        new OpenLayers.Strategy.Cluster()
 	    ],
 	    protocol: new OpenLayers.Protocol.HTTP({
 	        url: HABWatch.Flickr.URL,
-	        params: {
-	            format: "WFS",
-	            sort: "interestingness-desc",
-	            service: "WFS",
-	            request: "GetFeatures",
-	            srs: "EPSG:4326",
-	            maxfeatures: 150,
-	            bbox: [-180, -90, 180, 90]
-	        },
-	        format: new OpenLayers.Format.GML()
+	        format: new OpenLayers.Format.GeoJSON({
+                'internalProjection': new OpenLayers.Projection('EPSG:900913'),
+                'externalProjection': new OpenLayers.Projection('EPSG:4326')
+            })
 	    }),
 	    styleMap: new OpenLayers.StyleMap({
-	        "default": new OpenLayers.Style({
-	            pointRadius: "${radius}",
-	            fillColor: "#ffcc66",
+	        'default': new OpenLayers.Style({
+	            pointRadius: '${radius}',
+	            fillColor: '#67B1E6',
 	            fillOpacity: 0.8,
-	            strokeColor: "#cc6633",
+	            strokeColor: '#004E85',
 	            strokeWidth: 2,
 	            strokeOpacity: 0.8
 	        }, {
 	            context: {
 	                radius: function(feature) {
-	                    return Math.min(feature.attributes.count, 7) + 3;
+	                    return Math.min(feature.attributes.count, 10) + 5;
 	                }
 	            }
 	        }),
-	        "select": {
-	            fillColor: "#8aeeef",
-	            strokeColor: "#32a8a9"
+	        'select': {
+	            fillColor: '#0078CC',
+	            strokeColor: '#004E85'
 	        }
-	    })
+	    }),
+	    eventListeners: {
+	    	'loadstart': function(){
+	    		console.log('starting', $time());
+	    	},
+	    	'loadend': function(){
+	    		console.log('complete', $time());	
+	    	},
+	    	scope: this
+	    }
 	});
 	
 	var select = new OpenLayers.Control.SelectFeature(photos, {hover: true});
 	map.addControl(select);
 	map.addLayer(photos);
 	select.activate();
-	//photos.events.on({"featureselected": display});
+	//photos.events.on({'featureselected': display});
+    
 });
